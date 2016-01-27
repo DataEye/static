cdab5d7ff0e798ecea577aefa1a7790471b6af8d
simongfxu <xugaofan@gmail.com>
2016-01-27 17:40:27 +0800

    nothing to say

--- webpack/tracking.js
+++ webpack/tracking.js
@@ -50,5 +50,6 @@ module.exports = {
     }),
     new ExtractTextPlugin(config.STYLE_BUNDLE_PATH, {allChunks: true}),
     new webpack.optimize.CommonsChunkPlugin('vendor', config.VENDOR_BUNDLE_PATH)
-  ].concat(config.PLUGINS)
+  ].concat(config.PLUGINS),
+  devtool: '#source-map'
 }
