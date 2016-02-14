380d1eec1562bb095f3639f4cc8a22e32d787889
wendy wang <wendy wang>
2016-02-14 17:17:56 +0800

    fix bugs of add device

--- assets/monitor/pages/cmdb/device/addUpdateDevice.jsx
+++ assets/monitor/pages/cmdb/device/addUpdateDevice.jsx
@@ -13,17 +13,47 @@ export default React.createClass({
   componentWillMount: function() {
     this.props.actions.deviceInitSelectData()
   },
+
+  handleSubmit() {
+    let handler = this.refs.addUpdateDevice
+    handler.saveDevice.call(handler)
+  },
+
+  save(param) {
+    let id = Number(this.props.params.id)
+    if (id >= 0) {
+      this.props.actions.deviceUpdate(param)
+    } else {
+      this.props.actions.deviceAdd(param)
+    }
+  },
+  cancel() {
+    window.location.hash = '/configuration/devices'
+  },
+
   render() {
     return (
       <div className="content clearfix">
         <Loading loading={this.props.states.device.loadingSelectData}>
           <AddUpdateDeviceForm
+            ref="addUpdateDevice"
             id={this.props.params.id}
             initData={this.props.states.device.initData}
             actions={this.props.actions}
             states={this.props.states}
+            commitSubmit={this.save}
           />
         </Loading>
+        <div className="col-md-6">
+          <div className="form-group">
+            <div className="col-xs-offset-3 col-xs-7">
+              <button className="btn-custom btn-small btn-white pull-left"
+                      onClick={this.cancel}>取消</button>
+              <button className="btn-custom btn-small btn-blue pull-left"
+                      onClick ={this.handleSubmit}>确定</button>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
